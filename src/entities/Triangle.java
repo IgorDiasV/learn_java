2e414ffc6da2d8e36ae4492eae5e90d8626970b2
Igor Dias <igorverissimo02@hotmail.com>
2024-01-15 20:10:25 -0300

    adicionado um construtor a classe

--- src/entities/Triangle.java
+++ src/entities/Triangle.java
@@ -5,6 +5,12 @@ public class Triangle {
     public double b;
     public double c;
 
+    public Triangle(double a, double b, double c){
+        this.a = a;
+        this.b = b;
+        this.c = c;
+    }
+
     public double get_area(){
         double p = (a + b + c)/2.0;
         double result = Math.sqrt(p*(p-a)*(p-a)*(p-a));
