2e285c7b59345155db3f98e5cb36fe4fe68b53e5
Igor Dias <igorverissimo02@hotmail.com>
2024-01-18 17:25:56 -0300

    exemplo vetores java

---
+++ src/vetores_listas.java
@@ -0,0 +1,13 @@
+public class vetores_listas {
+
+    public static void main(String[] args) {
+
+        // vetores 
+        int[] numeros = new int[5];
+        numeros[0] = 67;
+        
+    
+        
+    }
+    
+}
