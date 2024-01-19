0e8f08662889b67566b588ecdf5abf7debcc62d0
Igor Dias <54191949+IgorDiasV@users.noreply.github.com>
2024-01-19 13:18:17 +0000

    exemplo inicial do uso de lista

--- src/vetores_listas.java
+++ src/vetores_listas.java
@@ -1,3 +1,5 @@
+import java.util.List;
+import java.util.ArrayList;
 public class vetores_listas {
 
     public static void main(String[] args) {
@@ -6,6 +8,11 @@ public class vetores_listas {
         int[] numeros = new int[5];
         numeros[0] = 67;
         
+                //  criando uma lista de strings
+        List<String> lista = new ArrayList<>(); 
+
+        // adicionando um elemento a lista
+        lista.add("João");
     
         
     }
