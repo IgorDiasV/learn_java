59c4e77f5c0eb4f8d5479e8665def5f5f31ba4c8
Igor Dias <54191949+IgorDiasV@users.noreply.github.com>
2024-01-19 16:38:37 +0000

    adicionando algumas funções das listas

--- src/vetores_listas.java
+++ src/vetores_listas.java
@@ -8,12 +8,32 @@ public class vetores_listas {
         int[] numeros = new int[5];
         numeros[0] = 67;
         
-                //  criando uma lista de strings
+
+        //  criando uma lista de strings
         List<String> lista = new ArrayList<>(); 
 
-        // adicionando um elemento a lista
+        // adicionando um elemento no final da lista
         lista.add("João");
-    
+
+        // adicionando um elemento em um posição especifica
+        lista.add(0, "Maria");
+
+        // retorna o tamanho da lista
+        System.out.println(lista.size());
+        // remove o primeiro elemento encontrado que contenha a string passada
+        lista.remove("Maria");
+
+        // remover o elemento do index 2
+        lista.remove(2);
+
+        // remove baseado em uma condição
+        lista.removeIf(x -> x == "Maria");
+
+        // informa o index da string passada
+        lista.indexOf("João");
+
+        // retorna uma lista filtrada pela condição passada
+        List<String> filtrado = lista.stream().filter( x -> x == "João").collect(Collectors.toList());
         
     }
     
